c3c23f34beef6856532780fef6a518207ba4de84
Nandini <nandinigupta63696@gmail.com>
2024-03-16 16:44:42 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import DataLedpart2 from './Components/DataLedpart2'
 import DataLedpart3 from './Components/DataLedpart3'
+import Datavideopart from './Components/Datavideopart'
 import DataLedpart4 from './Components/DataLedpart4'
 import FAQ from './Components/FAQ'
 import Footer from './Components/Footer'
@@ -12,10 +13,10 @@ function App() {
     <>
     <DataLedpart2/>
     <DataLedpart3/>
+    <Datavideopart/>
     <DataLedpart4/>
     <FAQ/>
-    <Footer/>
-      
+    <Footer/> 
     </>
   )
 }
